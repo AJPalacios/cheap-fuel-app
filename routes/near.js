02febcdab2a4b1db9ce66cd17cf95dfa3209874e
AJPalacios <ajpalacios.sist@gmail.com>
2018-10-15 15:24:10 -0500

    Se arreglo query

--- routes/near.js
+++ routes/near.js
@@ -3,8 +3,8 @@ const router  = express.Router();
 const gasPoint = require('../models/gasPoint')
 
 router.get('/list', (req,res,next)=>{
-
-  let query = {'geometry_2dsphere':{$near:{$geometry:{type:"Point",coordinates:[-99.004700, 19.260320]},$maxDistance:5000,$minDistance:0}}}
+  // {geometry: {$geoWithin: { $centerSphere: [ [ -102.6662884103825, 22.763007712120313 ], 0.014108282196571927 ]}}}
+  let query = {geometry:{$geoWithin:{$centerSphere:[[-99.004700, 19.260320],5000]}}}
 
   //gasPoint.createIndex({"location":"2dsphere"})
 
