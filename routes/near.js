a7e2499c4d5a0677eeb5aeb1e8efc23300be3a66
Adan Palacios <ajpalacios.sist@gmail.com>
2018-10-16 16:25:24 -0500

    mODIFICACION DE RUTAS EN APP

--- routes/near.js
+++ routes/near.js
@@ -2,21 +2,24 @@ const express = require('express');
 const router  = express.Router();
 const gasPoint = require('../models/gasPoint')
 
-router.get('/list', (req,res,next)=>{
-  // {geometry: {$geoWithin: { $centerSphere: [ [ -102.6662884103825, 22.763007712120313 ], 0.014108282196571927 ]}}}
-  let query = {geometry:{$geoWithin:{$centerSphere:[[-99.004700, 19.260320],5000]}}}
+router.get('/stations/', (req,res)=>{
+  console.log("QUERY DESDE MONGO"+req.query)
+  let longitude = req.query
+  console.log(longitude)
 
-  //gasPoint.createIndex({"location":"2dsphere"})
+  let query = {geometry:{$geoWithin:{$centerSphere:[[req.query.longitude, req.query.latitude],5 / 6378.1]}}}
 
   gasPoint.find(query)
-  .then(res=>{
-    //res.send(res)
-    console.log(res)
-  })
+  .then(data=>res.status(200).json(data))
   .catch(err=>{
     console.log(err)
   })
 
+  // {geometry: {$geoWithin: { $centerSphere: [ [ -102.6662884103825, 22.763007712120313 ], 0.014108282196571927 ]}}}
+})
+
+router.get('/new/',(req,res)=>{
+  res.send("hola")
 })
 
 module.exports = router;
