1b63dc9592c3b675538d859db14ea2215f0964b5
Adan Palacios <ajpalacios.sist@gmail.com>
2018-10-17 10:15:07 -0500

    Para prod

--- routes/near.js
+++ routes/near.js
@@ -21,8 +21,27 @@ router.get('/stations/', (req,res)=>{
   // {geometry: {$geoWithin: { $centerSphere: [ [ -102.6662884103825, 22.763007712120313 ], 0.014108282196571927 ]}}}
 })
 
-router.get('/new/',(req,res)=>{
-  res.send("hola")
+
+
+router.get('/stations/premium/', (req,res)=>{
+  console.log("QUERY DESDE MONGO"+req.query)
+  let longitude = req.query
+  console.log(longitude)
+
+  let query = {geometry:{$geoWithin:{$centerSphere:[[req.query.longitude, req.query.latitude],5/3959]}}}
+
+  gasPoint.find(query).sort({gas_price_premium:1}).limit(1)
+  .then(data=>{
+    res.status(200).json(data)
+    console.log(data)
+  })
+  .catch(err=>{
+    console.log(err)
+  })
+
+  // {geometry: {$geoWithin: { $centerSphere: [ [ -102.6662884103825, 22.763007712120313 ], 0.014108282196571927 ]}}}
 })
+
+
 
 module.exports = router;
